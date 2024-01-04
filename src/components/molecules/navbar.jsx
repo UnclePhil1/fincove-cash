098e47f9d1f18b46adce7dd778395d1732eb1b98
Unclephil <philipnssien@gmail.com>
2024-01-04 22:03:13 +0100

    navbar update

--- src/components/molecules/navbar.jsx
+++ src/components/molecules/navbar.jsx
@@ -1,21 +1,20 @@
-'use client'
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+"use client";
+import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
+import "../../../src/style.css";
 import Image from "next/image";
-import { MdMenu } from "react-icons/md";
+import Logo from "../../../public/Image/logo.png";
+import { MdMenu } from 'react-icons/md'
 
 const Navbar = () => {
   const router = useRouter();
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
   const handleShow = () => {
     setShow(!show);
-  };
-
-  const handleLinkClick = () => {
-    setShow(show);
-  };
+  }
 
   const isActive = (path) => {
     return router.pathname === path;
@@ -23,7 +22,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="flex justify-between items-center p-8 md:p-4 shadow-md bg-white z-10">
+      <nav className="flex justify-between items-center p-8 md:p-4 shadow-md bg-white z-10 ">
         <Link href="/" className="font-bold text-black self-start">
           <Image
             src={Logo}
@@ -33,42 +32,43 @@ const Navbar = () => {
             className="w-[120px] h-[50px]"
           />
         </Link>
-        <div
-          className={`${
-            show ? "block" : "hidden lg:flex"
-          } flex gap-10 justify-center md:justify-between items-center w-[60%] responsive`}
-        >
+        <div className={`${show ? 'block' : "hidden lg:flex"} flex gap-10 justify-center md:justify-between items-center w-[60%] responsive`}>
           <ul className="flex lg:space-x-[30px] flex-col lg:flex-row text-center self-center">
-            <Link href="/notFound" passHref onClick={handleLinkClick}
-              className=
-              {`font-medium links text-gray-500 hover:text-black ${
+           
+            <Link
+              href="/notFound"
+              className={`font-medium links text-gray-500 hover:text-black ${
                 isActive("/notFound") ? "text-black" : ""
-              }`}>
+              }`}
+            >
               About
             </Link>
-            <Link href="/notFound" passHref onClick={handleLinkClick}
-              className=
-              {`font-medium links text-gray-500 hover:text-black ${
+            <Link
+              href="/notFound"
+              className={`font-medium links text-gray-500 hover:text-black ${
                 isActive("/notFound") ? "text-black" : ""
-              }`}>
+              }`}
+            >
               FAQ
             </Link>
           </ul>
 
           <div className="flex flex-col md:flex-row gap-10">
-            <Link href={"/auth/signin"} passHref onClick={handleLinkClick}
-              className="md:flex text-white py-2 px-4 rounded signin"> Sign In
+            <Link
+              href={"/auth/signin"}
+              className="md:flex text-white py-2 px-4 rounded signin"
+            >
+              Sign In
             </Link>
-            <Link href={"/auth/signup"} passHref onClick={handleLinkClick}>
-              onClick={handleLinkClick}
-              className="md:flex text-white py-2 px-4 rounded signup" Sign Up
+            <Link
+              href={"/auth/signup"}
+              className="md:flex text-white py-2 px-4 rounded signup"
+            >
+              Sign Up
             </Link>
           </div>
         </div>
-        <button
-          onClick={handleShow}
-          className="mx-8 w-[30px] h-[30px] fixed lg:relative top-3 right-3 lg:hidden z-30"
-        >
+        <button onClick={handleShow} className="mx-8 w-[30px] h-[30px] fixed lg:relative top-3 right-3 lg:hidden z-30">
           <MdMenu size={34} />
         </button>
       </nav>
