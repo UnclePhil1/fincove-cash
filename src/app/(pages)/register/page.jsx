6aac292fb29eee5dcb24d1586e2be23338cc8d57
Unclephil <philipnssien@gmail.com>
2024-01-09 00:35:10 +0100

    updated

--- src/app/(pages)/register/page.jsx
+++ src/app/(pages)/register/page.jsx
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 
-export default async function Register() {
+export default async function Registers() {
   const session = await getServerSession(authOptions);
 
   if (session) redirect("/dashboard");
